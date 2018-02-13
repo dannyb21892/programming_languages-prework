af3e18624474d3f4d72a2cbbbe75935506fc6aef
Daniel Baamonde <dannyb21892@gmail.com>
2018-02-13 18:17:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(input)
   out = {}
   input.each do |style, languages|
     languages.each do |language, info|
-      out[language] = {:type => input[languages][language][:type], :style => []}
+      out[language] = {:type => input[style][language][:type], :style => []}
     end
   end
   input.each do |style, languages|
